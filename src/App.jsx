57593d6eb8b2754d3151de67cff8571d0e34bcd9
Khushboo Raikwar <j.khushbooraikwar95@gmail.com>
2022-03-24 10:02:56 +0530

    Added css files

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import Intro from "./components/intro/Intro";
+import Topbar from "./components/topbar/Topbar";
+import Portfolio from "./components/portfolio/Portfolio"
+import Testimonials from "./components/testimonials/Testimonials"
+import Works from "./components/works/Works";
+import Contact from "./components/contact/Contact";
+import './app.scss'
+
+function App() {
+  return (
+    <div className="app">
+     <Topbar/>
+     <div className="sections">
+     <Intro/>
+     <Portfolio/>
+     <Works/>
+     <Testimonials/>
+     <Contact/>
+     </div>
+    </div>
+  );
+}
+
+export default App;
--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,6 +1,6 @@
 import './contact.scss'
 
-export default function Contact () {
+export default function Contact() {
     return (
         <div className='contact'>
             
