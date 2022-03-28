18abeb5d6f909e877d139707317de826b63edbc7
Khushboo Raikwar <j.khushbooraikwar95@gmail.com>
2022-03-28 20:39:00 +0530

    added id to all files

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,8 +1,14 @@
-import './topbar.scss'
+import "./topbar.scss";
 
 export default function Topbar() {
   return (
-    <div className='topbar'>
+    <div className="topbar" id="topbar">
+      <div className="wrapper">
+        <div className="left">
+          <a href="#intro">SDfsf</a>
+        </div>
+        <div className="right"></div>
+      </div>
     </div>
-  )
+  );
 }
